39dc224a7b1e8d76e2cbb4e03122bc216bb83555
maslov <maslov@iac.spb.ru>
2023-08-25 23:27:37 +0300

    use ranker_service

--- main.py
+++ main.py
@@ -6,30 +6,45 @@ from fastapi import FastAPI, status
 import uvicorn
 
 class Answer():
-    def __init__(self, answerText: str, url: str) -> None:
+    def __init__(self, answerText: str, url: str = '') -> None:
         self.answerText=answerText
         self.url=url 
 
 
-# ranker_service_url = os.getenv('RANKER_SERVICE_URL')
-# if ranker_service_url is None:
-#     raise Exception(f'No RANKER_SERVICE_URL')
+ranker_service_url = os.getenv('RANKER_SERVICE_URL', default='http://127.0.0.1:8085')
+squad_service_url = os.getenv('SQUAD_SERVICE_URL', default='http://127.0.0.1:8090')
+
 
 app = FastAPI()
 
+# def find_result(question: str, text: str):
+#     response = requests.post(squad_service_url + '/squad', json= {
+#         "context": text,
+#         "question": question
+#     })
+#     response.json
+#     return 
+
 
 @app.get("/api/v1/question/")
-async def question(q: str = '', status_code=status.HTTP_200_OK):
+async def question(q: str = ''):
     result = []
 
-    if q == 'ошибка':
-        result.append(Answer(answerText='По вашему вопросу не удалось найти ответ, пожалуйста, попробуйте перефразировать вопрос и спросить повторно.', url=''))
-    else:
-        result.append(Answer(answerText='чудо ответ', url=''))
+    response = requests.get(ranker_service_url + '/find_similarity?question=' + q)
+    
+    answers = response.json()
+    if answers and len(answers) > 0:
+        for answer in answers:
+            if answer['type'] == 'question':
+                result.append(Answer(answerText=answer['a']))
+            elif answer['type'] == 'question_and_answer':
+                result.append(Answer(answerText=answer['a']))
+    else :
+        result.append(Answer(answerText='По вашему вопросу не удалось найти ответ, пожалуйста, попробуйте перефразировать вопрос и спросить повторно.'))
 
     return result
 
-@app.post("/api/v1/train", status_code=status.HTTP_201_CREATED)
+@app.post("/api/v1/train")
 async def train():
     return "OK"
 
