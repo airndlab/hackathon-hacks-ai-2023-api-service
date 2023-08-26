a04d54a80f22beca24da1085e8e9a5d2a23e0055
Grig Alex <alexengrigdev@gmail.com>
2023-08-26 19:22:03 +0300

    Add vectorizer

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ squad_service_url = os.getenv('SQUAD_SERVICE_URL', default='http://127.0.0.1:809
 
 is_vanilla_ranker_default = os.getenv('VANILLA_RANKER_DEFAULT', default='False').lower() in ('true', '1', 't')
 
+
 def find_in_squad(q: str, text: str):
     try:
         response = requests.post(squad_service_url + '/squad', json={
@@ -41,24 +42,22 @@ def find_in_squad(q: str, text: str):
         return {"answer": "Ошибка, не получилось найти ответ на Ваш вопрос ... ", "score": 0}
 
 
-def get_ranker_service_url(vanilla: bool):
-    if not vanilla:
-        return ranker_service_url
+def find_in_ranker(q: str, vanilla: bool, vectorizer: str = ''):
+
+    if vanilla:
+        url = f'{vanilla_ranker_service_url}/find_similarity?question={q}&vectorizer={vectorizer}'
     else:
-        return vanilla_ranker_service_url
+        url = f'{ranker_service_url}/find_similarity?question={q}'
 
-
-def find_in_ranker(q: str, vanilla: bool):
-    service_url = get_ranker_service_url(vanilla)
-    response = requests.get(service_url + '/find_similarity?question=' + q)
+    response = requests.get(url)
     return response.json()
 
 
 @app.get("/api/v1/question/")
-async def question(q: str = '', vanilla: bool = is_vanilla_ranker_default):
+async def question(q: str = '', vanilla: bool = is_vanilla_ranker_default, vectorizer: str | None = None):
     result = []
 
-    answers = find_in_ranker(q, vanilla)
+    answers = find_in_ranker(q, vanilla, vectorizer)
     if answers and len(answers) > 0:
         for answer in answers:
             if answer['type'] == 'question':
@@ -77,7 +76,7 @@ async def question(q: str = '', vanilla: bool = is_vanilla_ranker_default):
 
 @app.post("/api/v1/train")
 async def train(vanilla: bool = is_vanilla_ranker_default):
-    service_url = get_ranker_service_url(vanilla)
+    service_url = vanilla_ranker_service_url if vanilla else ranker_service_url
     return requests.post(service_url + '/train')
 
 
